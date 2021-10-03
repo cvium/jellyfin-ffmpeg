1bc5b7f33175776915322e78a069a264c54dd99d
cvium <clausvium@gmail.com>
2021-10-03 13:26:38 +0200

    stuff

--- fftools/ffkeyframes.c
+++ fftools/ffkeyframes.c
@@ -25,8 +25,15 @@
  * simple video keyframes extractor based on the FFmpeg libraries
  */
 
-#include "libavformat/avformat.h"
 #include "cmdutils.h"
+#include "libavformat/avformat.h"
+#include "libavformat/internal.h"
+
+#include "libavutil/common.h"
+#include "libavutil/avutil.h"
+
+#define TIME_BASE_TICKS 10000000
+#define TIME_BASE_Q_TICKS (AVRational){1, TIME_BASE_TICKS}
 
 const char program_name[] = "ffkeyframes";
 const int program_birth_year = 2021;
@@ -42,13 +49,15 @@ int main (int argc, char **argv)
 {
     const char *src_filename = NULL;
     int interval = 0;
-
+    int nb_keyframes = 0;
+    int (*read_packet)(struct AVFormatContext *, AVPacket *pkt) = NULL;
     AVPacket *pkt;
+    AVInputFormat *iformat;
     AVFormatContext *fmt_ctx = NULL;
 
     AVRational time_base;
     int ret, i = 0, video_stream_idx = -1;
-    int64_t scaled_interval, timestamp = 0, last_pts = -1;
+    int64_t scaled_interval, timestamp = 0, last_pts = 0, scaled_pts = -1;
 
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <video> <interval>\n", argv[0]);
@@ -58,16 +67,20 @@ int main (int argc, char **argv)
     src_filename = argv[1];
     interval = strtol(argv[2], NULL, 10);
 
-    if ((ret = avformat_open_input(&fmt_ctx, src_filename, NULL, NULL)) < 0) {
+    iformat = av_find_input_format(src_filename);
+
+    if ((ret = avformat_open_input(&fmt_ctx, src_filename, iformat, NULL)) < 0) {
         fprintf(stderr, "Could not open source file %s\n", src_filename);
         goto end;
     }
 
+    video_stream_idx = av_find_best_stream(fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
+
     // discard all non-video streams to make it slightly faster
     for (i = 0; i < fmt_ctx->nb_streams; i++)
     {
-        if (fmt_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO && video_stream_idx < 0) {
-            video_stream_idx = i;
+        if (video_stream_idx == i) {
+            // video_stream_idx = i;
             fmt_ctx->streams[i]->discard = AVDISCARD_NONKEY;
             time_base = fmt_ctx->streams[video_stream_idx]->time_base;
         } else {
@@ -77,6 +90,7 @@ int main (int argc, char **argv)
 
     // exit if we didn't find a video stream
     if ((ret = video_stream_idx) < 0) {
+        fprintf(stderr, "Could not find video stream\n");
         goto end;
     }
 
@@ -86,24 +100,51 @@ int main (int argc, char **argv)
         time_base.den,
         time_base.num
     );
+    
+    /* retrieve stream information */
+    if ((ret = avformat_find_stream_info(fmt_ctx, NULL)) < 0) {
+        fprintf(stderr, "Could not find stream information\n");
+        goto end;
+    }
 
-    while (avformat_seek_file(fmt_ctx, video_stream_idx, timestamp, timestamp, timestamp, 0) >= 0) {
-        if (av_read_frame(fmt_ctx, pkt) < 0) {
+    if (fmt_ctx->streams[video_stream_idx]->duration != AV_NOPTS_VALUE) {
+        printf("duration:%ld\n", av_rescale_q(fmt_ctx->streams[video_stream_idx]->duration, time_base, TIME_BASE_Q_TICKS));
+    } else {
+        printf("duration:%ld\n", av_rescale_q(fmt_ctx->duration, AV_TIME_BASE_Q, TIME_BASE_Q_TICKS));
+    }
+
+    do {
+        // set up the read_packet pointer
+        if (read_packet == NULL) {
+            if (ff_read_packet(fmt_ctx, pkt) < 0) {
+                break;
+            }
+
+            // if pts is AV_NOPTS_VALUE we fall back to the slower, but more accurate av_read_frame
+            read_packet = pkt->pts == AV_NOPTS_VALUE ? av_read_frame : ff_read_packet;
+        } else if (read_packet(fmt_ctx, pkt) < 0) {
             break;
         }
 
-        if (pkt->flags & AV_PKT_FLAG_KEY && last_pts < pkt->pts) {
-            last_pts = pkt->pts;
-            timestamp = pkt->pts;
-            printf("%lld\n", pkt->pts);
+        scaled_pts = av_rescale_q(pkt->pts, time_base, TIME_BASE_Q_TICKS);
+
+        if (pkt->flags & AV_PKT_FLAG_KEY && last_pts < scaled_pts) {
+            nb_keyframes++;
+            last_pts = scaled_pts;
+            timestamp = pkt->pts + 1;
+
+            printf("%ld\n", last_pts);
+        } else {
+            timestamp = FFMAX(timestamp, pkt->pts) + scaled_interval;
         }
 
-        timestamp += scaled_interval;
         av_packet_unref(pkt);
-    }
+    } while (av_seek_frame(fmt_ctx, video_stream_idx, timestamp, 0) >= 0);
+
+    printf("keyframes_count:%d\n", nb_keyframes);
 
     // it is assumed to have been successful if we found at least one keyframe
-    ret = timestamp > 0;
+    ret = nb_keyframes > 0;
 
 end:
     avformat_close_input(&fmt_ctx);
